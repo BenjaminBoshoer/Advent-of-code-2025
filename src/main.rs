0c2cd4298e2d9908552d31ae07eef07ff52c12d0
BenjaminBoshoer <boshoer.beny@gmail.com>
2025-12-12 15:49:35 +0200

    ADDED: solution for day 2 part 1 is added

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 mod solutions;
 
 fn main() {
-    println!("[+] Calling Day 1 solution function");
+    /*println!("[+] Calling Day 1 solution function");
     match solutions::day_1() {
-        Ok(n) => println!("Solution is {}", n),
+        Ok(n) => println!("[!] Solution is {}", n),
+        Err(s) => println!("{}", s),
+    }*/
+
+    println!("[+] Calling Day 2 solution function");
+    match solutions::day_2() {
+        Ok(n) => println!("[!] Solution is {}", n),
         Err(s) => println!("{}", s),
     }
 }
--- src/solutions.rs
+++ src/solutions.rs
@@ -1,5 +1,5 @@
 use std::{
-    fs::File,
+    fs::{self, File},
     io::{BufRead, BufReader},
 };
 
@@ -29,43 +29,76 @@ pub fn day_1() -> Result<i32, String> {
         // Use the match keyword to distinct between R and L behavior
         match direction {
             "R" => {
-                print!("{}{}: {} ", direction, degree, pointer);
+                //print!("{}{}: {} ", direction, degree, pointer);
 
                 if pointer + (degree % 100) > 99 {
                     clicks += 1;
-                    print!("click ");
+                    //print!("click ");
                 } 
 
                 pointer = (pointer + (degree % 100)) % 100;
 
-                print!("-> {}", pointer);
+                //print!("-> {}", pointer);
                 clicks += degree / 100;
                 
-                println!(" Added {} clicks due to rotations", degree/100);
+                //println!(" Added {} clicks due to rotations", degree/100);
 
             }
             "L" => {
-                print!("{}{}: {} ->", direction, degree, pointer);
+                //print!("{}{}: {} ->", direction, degree, pointer);
                 if pointer == 0 {
                     pointer = 100 - (degree.abs() % 100);
                 } else if pointer - (degree.abs() % 100) < 0{
                     clicks += 1;
-                    print!(" Click ");
+                    //print!(" Click ");
                     pointer = 100 - (pointer - (degree.abs() % 100)).abs() % 100;
                 } else if pointer - (degree.abs() % 100) == 0{
                     clicks += 1;
-                    print!(" Click ");
+                    //print!(" Click ");
                     pointer = 0;
                 } else {
                     pointer = pointer - (degree % 100);
                 }
-                print!(" {}", pointer);
+                //print!(" {}", pointer);
                 clicks += degree / 100;
-                println!(" Added {} clicks due to rotations", degree/100);
+                //println!(" Added {} clicks due to rotations", degree/100);
             }
             _ => {}
         }
     }
 
     Ok(clicks)
+}
+
+
+pub fn day_2() -> Result<i64, String> {
+    //Read the file inputs/day_1.txt. On error return the error string.
+    let buffer = match fs::read_to_string("inputs/day_2.txt") {
+        Ok(n) => n,
+        Err(e) => {
+            println!("Error opening file:");
+            return Err(e.to_string());
+        }
+    };
+    let mut sum: i64 = 0;
+    let ranges: Vec<_> = buffer.split(',').collect();
+
+    for range in ranges {
+        let v: Vec<_> = range.split('-').collect();
+        let start = v[0].trim().parse::<i64>().unwrap();
+        let end = v[1].trim().parse::<i64>().unwrap();
+
+        let mut ptr = start;
+        for _ in 0..=(end-start) {
+            let len = ptr.to_string().len();
+            if len % 2 == 0 {
+                if ptr.to_string()[0..len/2] == ptr.to_string()[len/2..] {
+                    sum += ptr;
+                    println!("invalid ID {}", ptr);
+                }
+            }
+            ptr += 1
+        }
+    }
+    Ok(sum)
 }
